46bfd156e938bd992aeb6aaa5ebe33affded7a84
RAHUL <dasr16583@gmail.com>
2023-07-09 23:57:06 +0530

    complete working on page 6 and its animation

--- script.js
+++ script.js
@@ -382,3 +382,27 @@ function canvas1() {
   });
 }
 canvas1();
+
+
+var clutter = "";
+
+document
+  .querySelector("#page6>h1")
+  .textContent.split("")
+  .forEach(function (dets) {
+    clutter += `<span>${dets}</span>`;
+
+    document.querySelector("#page6>h1").innerHTML = clutter;
+  });
+
+gsap.to("#page6>h1>span", {
+  scrollTrigger: {
+    trigger: `#page6>h1>span`,
+    start: `top bottom`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: 0.5,
+  },
+  stagger: 0.2,
+  color: `#fff`,
+});
